6140ee92acadd67730c0a584471adcf228a8a92b
Martin Nuñez Igarzabal <martin.mr1367@gmail.com>
2026-02-11 20:07:55 -0300

    Update
    Red ending flag

--- components/Ruteo.js
+++ components/Ruteo.js
@@ -13,6 +13,12 @@ export function Ruteo(
         map.removeSource('ruta_source');
     }
 
+    // Remover marcador de llegada anterior si existe
+    const marcadorAnterior = document.querySelector('[data-llegada-marker]');
+    if (marcadorAnterior) {
+        marcadorAnterior.remove();
+    }
+
     //Con esto llamo a la api de ruteo de location IQ. Luego invoco las propiedades geometry y distance.
     fetch(`https://us1.locationiq.com/v1/directions/driving/${longitudUsuario},${latitudUsuario};${longitudPOI},${latitudPOI}?key=${YOUR_ACCESS_TOKEN}&geometries=geojson`)
         .then((response) => response.json())
@@ -36,6 +42,20 @@ export function Ruteo(
                     'line-width': 4,
                 }
             })
+
+            // Crear elemento HTML para la banderita roja de llegada
+            const elementoBanderita = document.createElement('div');
+            elementoBanderita.innerHTML = '🚩';
+            elementoBanderita.style.fontSize = '32px';
+            elementoBanderita.style.cursor = 'pointer';
+            elementoBanderita.setAttribute('data-llegada-marker', 'true');
+
+            // Agregar marcador en el punto final de la ruta
+            new maplibregl.Marker({
+                element: elementoBanderita
+            })
+                .setLngLat([longitudPOI, latitudPOI])
+                .addTo(map);
 
         })
 }
